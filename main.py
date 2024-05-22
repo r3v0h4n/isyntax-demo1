4953c1a1c775a2d8b7cda895b1981ebc67504824
Idonets <Idonets@morebis.net>
2024-05-22 21:57:19 +0300

    added email functionality

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from commands.AllCommand import AllCommand
 from commands.AddBirthdayCommand import AddBirthdayCommand
 from commands.ShowBirthdayCommand import ShowBirthdayCommand
 from commands.BirthdaysCommand import BirthdaysCommand
+from commands.AddEmailCommand import AddEmailCommand
+from commands.GetEmailCommand import GetEmailCommand
 
 def save_data(book, filename="addressbook.pkl"):
     with open(filename, "wb") as f:
@@ -30,7 +32,19 @@ def parse_input(user_input):
 def main():
     
     book = load_data()
-    commands = [HelloCommand(), AddContactCommand(), GetPhoneCommand(), ChangeContactCommand(), AllCommand(), AddBirthdayCommand(), ShowBirthdayCommand(), BirthdaysCommand()]
+    commands =\
+    [
+        HelloCommand(),
+        AddContactCommand(),
+        GetPhoneCommand(),
+        ChangeContactCommand(),
+        AllCommand(),
+        AddBirthdayCommand(),
+        ShowBirthdayCommand(),
+        BirthdaysCommand(),
+        GetEmailCommand(),
+        AddEmailCommand()
+    ]
 
     while True:
         user_input = input("Enter a command: ")
--- record.py
+++ record.py
@@ -1,12 +1,14 @@
 from name import Name
 from phone import Phone
 from birthday import Birthday
+from contact_email import Email
 
 class Record:
     def __init__(self, name: str) -> None:
         self.__name = Name(name)
         self.__phones = []
         self.__birthday = None
+        self.__emails = []
 
     def add_birthday(self, value: str):
         self.__birthday = Birthday(value)
@@ -26,6 +28,18 @@ class Record:
     def edit_phone(self, phone_to_edit: str, phone: str) -> None:
         self.find_phone(phone_to_edit).value = phone
 
+    def add_email(self, email: str) -> None:
+        self.__emails.append(Email(email))
+
+    def find_email(self, value: str) -> Email:
+        try:
+            return next(email for email in self.__emails if email.value == value)
+        except StopIteration:
+            raise ValueError(f"Contact {self.__name} doesn't have email {value}!")
+
+    def edit_email(self, email_to_edit: str, new_email: str) -> None:
+        self.find_email(email_to_edit).value = new_email
+
     def __str__(self) -> str:
         return f"Contact name: {self.__name}, birthday: {self.__birthday}, phones: {'; '.join(str(phone) for phone in self.__phones)}"
     
@@ -40,3 +54,7 @@ class Record:
     @property
     def phones(self) -> list:
         return self.__phones
+
+    @property
+    def emails(self) -> list:
+        return self.__emails
