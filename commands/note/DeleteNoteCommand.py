ed2593d174547cd675a11d17904e712773f54a6d
Idonets <Idonets@morebis.net>
2024-05-24 18:54:04 +0300

    update serch note

--- commands/note/DeleteNoteCommand.py
+++ commands/note/DeleteNoteCommand.py
@@ -10,5 +10,5 @@ class DeleteNoteCommand(NoteCommand):
         note = notebook.find(title)
         if not note:
             raise ValueError(f"Note with title: '{title}' is not found.")
-        notebook.remove()
+        notebook.remove(title)
         return f"Note titled '{title}' has been removed."
